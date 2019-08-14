e1670669b54b6bc93b4938c19475d82d5d2e73d7
Hiram Chirino <hiram@hiramchirino.com>
2019-08-14 16:32:41 -0400

    feat: Added windows support feat: Added -f flag to display a file feat: Added a better usage display.

--- cli.go
+++ cli.go
@@ -2,34 +2,58 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"github.com/k0kubun/go-ansi"
+	"github.com/mattn/go-isatty"
 	"os"
 )
 
-func main() {
-	info, err := os.Stdin.Stat()
-	if err != nil {
-		fmt.Printf("Error reading command pipe: %s\n", err)
+var inputFileName string = "-"
 
-		if os.Getenv("TESTING") == "" {
-			os.Exit(1)
-		}
-		return
+func main() {
+	flag.StringVar(&inputFileName, "f", "-", "file to process or '-' to process stdin")
+	flag.Parse()
+
+	in := os.Stdin
+	out := ansi.NewAnsiStdout()
+	if inputFileName != "-" {
+		f, err := os.Open(inputFileName)
+		exitOnUsageError(err)
+		in = f
+		defer in.Close()
 	}
 
-	if (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
-		fmt.Println("The command is intended to work with pipes.")
-		fmt.Println("Usage:")
-		fmt.Println("  cat yourlogfile.log | zapper")
-	} else if info.Size() > 0 {
-		reader := bufio.NewReader(os.Stdin)
+	if isatty.IsTerminal(in.Fd()) || isatty.IsCygwinTerminal(in.Fd()) {
+		exitOnUsageError(fmt.Errorf("the command is intended to work with pipes or files"))
+	}
 
-		zap := zapper{reader, os.Stdout}
-		zap.pipe()
+	reader := bufio.NewReader(in)
+	zap := zapper{reader, out}
+	err := zap.pipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s", err)
+		os.Exit(2)
+	}
+}
 
-		if os.Getenv("TESTING") == "" {
-			os.Exit(0)
-		}
-		return
+func exitOnUsageError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, `Usage: 
+
+  zapper [options]
+
+Options:
+  -f string
+        file to process or '-' to process stdin (default "-")
+
+Examples:
+  cat yourlogfile.log | zapper
+  zapper < yourlogfile.log
+  zapper -f yourlogfile.log
+
+`)
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/buger/jsonparser v0.0.0-20181115193947-bf1c66bbce23
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0
+	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213
 	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
 )
